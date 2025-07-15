77ec4b7dfa9ebd8108fc83aed6284ab920db4855
elvincedik <elvinse3@gmail.com>
2025-07-15 06:34:36 +0100

    comment scoping for permission table to fix dashboard bug

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -19,17 +19,17 @@ class Permission extends Model
      * @param  mixed $role
      * @return boolean
      */
-    public function organization()
-    {
-        return $this->belongsTo(Organization::class);
-    }
+    // public function organization()
+    // {
+    //     return $this->belongsTo(Organization::class);
+    // }
 
-    protected static function booted()
-    {
-        static::addGlobalScope('organization', function ($builder) {
-            if (auth()->check()) {
-                $builder->where('organization_id', auth()->user()->organization_id);
-            }
-        });
-    }
+    // protected static function booted()
+    // {
+    //     static::addGlobalScope('organization', function ($builder) {
+    //         if (auth()->check()) {
+    //             $builder->where('organization_id', auth()->user()->organization_id);
+    //         }
+    //     });
+    // }
 }
